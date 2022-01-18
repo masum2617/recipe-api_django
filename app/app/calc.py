5d58a5376d860f1f48fd42205ccb990d2a7e2c25
masum2617 <masum2617@gmail.com>
2022-01-18 23:18:19 +0600

    updated super user

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+
+def add(x,y):
+    """ Adding two num """
+    return x+y
+
+
+def subtract(x,y):
+    return y-x
---
+++ app/app/tests.py
@@ -0,0 +1,12 @@
+
+# from django.test import TestCase
+# from app.calc import add,subtract
+
+# class CalcTests(TestCase):
+#     def test_add_num(self):
+#         """ Test two numbers added together """
+#         self.assertEqual(add(3,8),11)
+
+#     def test_subtract_numbers(self):
+#         """ test subtracted values are returned """
+#         self.assertEqual(subtract(5,11),6)
